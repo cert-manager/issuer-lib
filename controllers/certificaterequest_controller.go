2e41c93f94917c6d55584a383abaed7f442dfaef
Tim Ramlot <42113979+inteon@users.noreply.github.com>
2023-05-26 16:31:55 +0200

    improvements based on review feedback
    Signed-off-by: Tim Ramlot <42113979+inteon@users.noreply.github.com>

--- controllers/certificaterequest_controller.go
+++ controllers/certificaterequest_controller.go
@@ -436,11 +436,18 @@ func (r *CertificateRequestReconciler) SetupWithManager(ctx context.Context, mgr
 		// API server. See:
 		// * https://github.com/kubernetes-sigs/controller-runtime/issues/562
 		// * https://github.com/kubernetes-sigs/controller-runtime/issues/1219
-		cacheSyncCTX, cancel := context.WithTimeout(ctx, mgr.GetControllerOptions().CacheSyncTimeout)
+		//
+		// The defaulting logic is based on:
+		// https://github.com/kubernetes-sigs/controller-runtime/blob/30eae58f1b984c1b8139dd9b9f68dd2d530ed429/pkg/controller/controller.go#L138-L144
+		timeout := mgr.GetControllerOptions().CacheSyncTimeout
+		if timeout == 0 {
+			timeout = 2 * time.Minute
+		}
+		cacheSyncCtx, cancel := context.WithTimeout(ctx, timeout)
 		defer cancel()
 
 		resourceHandler, err := kubeutil.NewLinkedResourceHandler(
-			cacheSyncCTX,
+			cacheSyncCtx,
 			mgr.GetLogger(),
 			mgr.GetScheme(),
 			mgr.GetCache(),
