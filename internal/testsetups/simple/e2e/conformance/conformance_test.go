e421edde8323ed6e0912e16d37652f40693ea2fd
Tim Ramlot <42113979+inteon@users.noreply.github.com>
2023-06-09 13:25:45 +0200

    resolve CI error
    Signed-off-by: Tim Ramlot <42113979+inteon@users.noreply.github.com>

--- internal/testsetups/simple/e2e/conformance/conformance_test.go
+++ internal/testsetups/simple/e2e/conformance/conformance_test.go
@@ -3,11 +3,15 @@ package conformance
 import (
 	"testing"
 
+	"github.com/cert-manager/issuer-lib/internal/tests/testcontext"
+	"github.com/cert-manager/issuer-lib/internal/tests/testresource"
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 )
 
 func TestConformance(t *testing.T) {
+	_ = testresource.EnsureTestDependencies(t, testcontext.ForTest(t), testresource.EndToEndTest)
+
 	gomega.RegisterFailHandler(ginkgo.Fail)
 
 	ginkgo.RunSpecs(t, "cert-manager conformance suite")
