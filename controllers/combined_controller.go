fd2e3e81898456b66148f1d12ca9a6a3d43e9224
hjoshi123 <mail@hjoshi.me>
2025-11-06 09:15:55 -0700

    fix missing ignore issuers logic
    Signed-off-by: hjoshi123 <mail@hjoshi.me>

--- controllers/combined_controller.go
+++ controllers/combined_controller.go
@@ -138,6 +138,7 @@ func (r *CombinedController) SetupWithManager(ctx context.Context, mgr ctrl.Mana
 
 				Client:                   cl,
 				Sign:                     r.Sign,
+				IgnoreIssuer:             r.IgnoreIssuer,
 				IgnoreCertificateRequest: r.IgnoreCertificateRequest,
 				EventRecorder:            r.EventRecorder,
 				Clock:                    r.Clock,
@@ -164,6 +165,7 @@ func (r *CombinedController) SetupWithManager(ctx context.Context, mgr ctrl.Mana
 
 				Client:                   cl,
 				Sign:                     r.Sign,
+				IgnoreIssuer:             r.IgnoreIssuer,
 				IgnoreCertificateRequest: r.IgnoreCertificateRequest,
 				EventRecorder:            r.EventRecorder,
 				Clock:                    r.Clock,
